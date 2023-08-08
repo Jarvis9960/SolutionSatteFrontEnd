08ca87862452369cd0906dc6d45a2869df4b6a12
SoutionSathee <139756555+SoutionSathee@users.noreply.github.com>
2023-08-08 23:33:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  server: {
+    port: 8080
+  },
   define: {
     'process.env': process.env
   },
