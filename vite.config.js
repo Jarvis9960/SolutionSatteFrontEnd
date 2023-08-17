536064d18e5c920d4f6fcccca2bda27c567b4d68
Jarvis9960 <ankitfukte11@gmail.com>
2023-08-17 12:09:33 +0530

    changing api's to production domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  },
   define: {
     'process.env': process.env
   },
